0ecc62793861e50d5591f8e613b6fc18d1d3d320
RuoYi <yzz_ivy@163.com>
2025-04-13 09:49:11 +0800

    update vue3

--- App.vue
+++ App.vue
@@ -1,30 +1,33 @@
-<script>
+<script setup>
+  import { onLaunch } from '@dcloudio/uni-app'
   import config from './config'
   import store from '@/store'
   import { getToken } from '@/utils/auth'
+  import { getCurrentInstance } from "vue"
 
-  export default {
-    onLaunch: function() {
-      this.initApp()
-    },
-    methods: {
-      // 初始化应用
-      initApp() {
-        // 初始化应用配置
-        this.initConfig()
-        // 检查用户登录状态
-        //#ifdef H5
-        this.checkLogin()
-        //#endif
-      },
-      initConfig() {
-        this.globalData.config = config
-      },
-      checkLogin() {
-        if (!getToken()) {
-          this.$tab.reLaunch('/pages/login') 
-        }
-      }
+  const { proxy } = getCurrentInstance()
+
+  onLaunch(() => {
+    initApp()
+  })
+
+  // 初始化应用
+  function initApp() {
+    // 初始化应用配置
+    initConfig()
+    // 检查用户登录状态
+    //#ifdef H5
+    checkLogin()
+    //#endif
+  }
+
+  function initConfig() {
+    getApp().globalData.config = config
+  }
+
+  function checkLogin() {
+    if (!getToken()) {
+      proxy.$tab.reLaunch('/pages/login') 
     }
   }
 </script>
