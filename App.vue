fd97380edb82499d6fc7510b36b7695fff80796e
RuoYi <yzz_ivy@163.com>
2025-04-23 10:37:50 +0800

    兼容vue3模式获取config

--- App.vue
+++ App.vue
@@ -1,7 +1,5 @@
 <script setup>
   import { onLaunch } from '@dcloudio/uni-app'
-  import config from './config'
-  import store from '@/store'
   import { getToken } from '@/utils/auth'
   import { getCurrentInstance } from "vue"
 
@@ -13,21 +11,25 @@
 
   // 初始化应用
   function initApp() {
-    // 初始化应用配置
-    initConfig()
     // 检查用户登录状态
     //#ifdef H5
     checkLogin()
     //#endif
   }
 
-  function initConfig() {
-    getApp().globalData.config = config
-  }
-
   function checkLogin() {
     if (!getToken()) {
       proxy.$tab.reLaunch('/pages/login') 
+    }
+  }
+</script>
+
+<script>
+  import config from './config'
+
+  export default {
+    onLaunch: function() {
+      this.globalData.config = config
     }
   }
 </script>
