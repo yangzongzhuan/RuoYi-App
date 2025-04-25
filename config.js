8461a8c7239fe5c0873eba835d1cfbded3598656
RuoYi <yzz_ivy@163.com>
2025-04-25 13:06:49 +0800

    update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 应用全局配置
 export default {
-  // baseUrl: 'https://vue.ruoyi.vip/prod-api',
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://vue.ruoyi.vip/prod-api',
+  // baseUrl: 'http://localhost:8080',
   // 应用信息
   appInfo: {
     // 应用名称
