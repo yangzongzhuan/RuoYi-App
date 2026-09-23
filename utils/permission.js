5256b2c6ffe46c6043a571563374b83667fd92e5
RuoYi <yzz_ivy@163.com>
2026-09-23 21:16:37 +0800

    update permission vue3

--- utils/permission.js
+++ utils/permission.js
@@ -1,4 +1,4 @@
-import store from '@/store'
+import { useUserStore } from '@/store/modules/user'
 
 /**
  * 字符权限校验
@@ -7,18 +7,17 @@ import store from '@/store'
  */
 export function checkPermi(value) {
   if (value && value instanceof Array && value.length > 0) {
-    const permissions = store.getters && store.getters.permissions
+    const permissions = useUserStore().permissions || []
     const permissionDatas = value
-    const all_permission = "*:*:*"
+    const all_permission = '*:*:*'
+
+    if (!permissions.length) return false
 
     const hasPermission = permissions.some(permission => {
       return all_permission === permission || permissionDatas.includes(permission)
     })
 
-    if (!hasPermission) {
-      return false
-    }
-    return true
+    return hasPermission
   } else {
     console.error(`need roles! Like checkPermi="['system:user:add','system:user:edit']"`)
     return false
@@ -32,18 +31,17 @@ export function checkPermi(value) {
  */
 export function checkRole(value) {
   if (value && value instanceof Array && value.length > 0) {
-    const roles = store.getters && store.getters.roles
+    const roles = useUserStore().roles || []
     const permissionRoles = value
-    const super_admin = "admin"
+    const super_admin = 'admin'
+
+    if (!roles.length) return false
 
     const hasRole = roles.some(role => {
       return super_admin === role || permissionRoles.includes(role)
     })
 
-    if (!hasRole) {
-      return false
-    }
-    return true
+    return hasRole
   } else {
     console.error(`need roles! Like checkRole="['admin','editor']"`)
     return false
